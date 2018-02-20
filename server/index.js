3b747cf4092a91abdbcd2bf8d5208dc2b1b89f40
Jose Naranjo <naranjja@gmail.com>
2018-02-19 20:05:17 -0500

    :rocket: Update routing environments

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ require("dotenv").config()
 
 const express = require("express")
 const bodyParser = require("body-parser")
+const path = require("path")
 
 const app = express()
+const root = path.resolve(__dirname, "..")
+
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -14,6 +18,16 @@ require("./api")(app, [
     "query"
 ])
 
-app.listen(process.env.EXPRESS_PORT, () => {
-    console.log(`Express API listening on port ${process.env.EXPRESS_PORT}!`)
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(root, "client", "build")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(root, "client", "build", "index.html"))
+    })
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}!`)
+    })
+} else {
+    app.listen(port, () => {
+        console.log(`API listening on port ${port}!`)
+    })
+}
